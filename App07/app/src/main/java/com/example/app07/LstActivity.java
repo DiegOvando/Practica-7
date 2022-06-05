f260e8c36beae82ad58e4c322bbb3c7dacd5cbc4
Diego Ovando <c9sneaky222@hotmail.com>
2022-06-05 14:10:07 -0600

    SearchView
    Se implementó el SearchView en la practica.

--- App07/app/src/main/java/com/example/app07/LstActivity.java
+++ App07/app/src/main/java/com/example/app07/LstActivity.java
@@ -3,12 +3,19 @@ package com.example.app07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import org.w3c.dom.Text;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LstActivity extends AppCompatActivity {
@@ -16,11 +23,14 @@ public class LstActivity extends AppCompatActivity {
     private TextView lblUser;
     private ListView lstAlumnos;
     private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
+    private SearchView srcLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lst);
+        srcLista= (SearchView) findViewById(R.id.menu_search);
         lblUser = (TextView) findViewById(R.id.lblUsuario);
         lstAlumnos = (ListView) findViewById(R.id.lstAlumnos);
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,
@@ -35,4 +45,23 @@ public class LstActivity extends AppCompatActivity {
         lblUser.setText(user.getNombreCompleto());
 
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.searchview,menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
--- App07/app/src/main/java/com/example/app07/MainActivity.java
+++ App07/app/src/main/java/com/example/app07/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
